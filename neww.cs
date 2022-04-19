add16a237caf7b29c88d3e8b505b77ae0d5e97ee
S-Dhir <73019620+S-Dhir@users.noreply.github.com>
2022-04-19 17:39:55 +0530

    Commenting
    actually made it easy for others to understand this

--- neww.cs
+++ neww.cs
@@ -1,4 +1,13 @@
-﻿using System;
+//I had also installed the Microsoft.Windows.Comaptibility nuget package
+//https://www.nuget.org/packages/Microsoft.Windows.Compatibility/7.0.0-preview.3.22175.4
+//
+// PLEASE DO THIS:
+//
+//It may work w/out it but I highly recommend installing it
+//The code installs the File in a random location- kge.t
+//You also have to set the values of URL and inbytes to the URL and the size of the file respectively
+// Please read the entire thing. It's just an example of one simple way to this stuff
+using System;
 using System.IO;
 using System.Diagnostics;
 using System.Text;
@@ -19,15 +28,15 @@ namespace notify
 {
     class Neww
     {
-        public static string URL = "https://raw.githubusercontent.com/S-Dhir/blog/main/webmap.txt";
-        public static long inbytes = 216;
-        public static Thread? le;
-        public static bool runsThread = true;
-        //        
-        public static void Main(string?[]? args)
+        public static string URL = ""; // You must enter the url here
+        public static long inbytes = ; // and the webpage size
+        public static Thread? le;//A thread that continously displays the amount of data downnloaded
+        public static bool runsThread = true;//Whether the "Downloading..." thing should be displayed or not. Once the file is downloaded this is set to false in line 100
+      
+        public static void Main(string?[]? args)// You might want to change this
         {
-            Console.CursorVisible = false;
-            if (!File.Exists("kge.t")) File.WriteAllText("kge.t", "", Encoding.Unicode);
+            Console.CursorVisible = false; // Make the cursor invisible so it's not visible
+            if (!File.Exists("kge.t")) File.WriteAllText("kge.t", "", Encoding.Unicode); //Creating the file
             le = new Thread(() =>
             {
 
@@ -36,18 +45,22 @@ namespace notify
                     Thread.Sleep(500);
                     if (runsThread == false)
                     {
-                        return;
+                        return;// one way to exit the thread
                     }
 
                     Console.SetCursorPosition(0, 1);
-                    Console.Write(Math.Round(((double)new FileInfo("kge.t").Length/double.Parse(inbytes.ToString()) * 100D), 2) + "%");
+                    int decimalPlaces = 2; //Increase this value if you are making it download a very very large file
+                    Console.Write(Math.Round(((double)new FileInfo("kge.t").Length/double.Parse(inbytes.ToString()) * 100D), decimalPlaces) + "%");
                 }
             });
             le.SetApartmentState(ApartmentState.MTA);
             le.Start();
             new Thread(() => 
             {
-                bool[] bits = { false, false };
+                //This thread just makes a cool display of the fact that it's downloading data.
+                
+                bool[] bits = { false, false };//This trick saves a ton of memory. This is the closes representation in C# to 00
+                //Which is a binary digit
                 while(runsThread)
                 {
                     if(!runsThread)
@@ -59,23 +72,23 @@ namespace notify
                     Console.SetCursorPosition(0, 0);
                     Console.Write(new string(' ', Console.BufferWidth));
                     Console.SetCursorPosition(0, 0);
-                    if(!bits[0] && !bits[1])
+                    if(!bits[0] && !bits[1])//00
                     {
                         Console.Write("Downloading");
                         bits[0] = true;
                         bits[1] = false;
                         Thread.Sleep(300);
-                    } else if(bits[0] && !bits[1])
+                    } else if(bits[0] && !bits[1])//10
                     {
                         bits[0] = false;
                         bits[1] = true;
                         Console.Write("Downloading.");
-                    } else if(!bits[0] && bits[1])
+                    } else if(!bits[0] && bits[1])//01
                     {
                         bits[0] = true;
                         bits[1] = true;
                         Console.Write("Downloading..");
-                    } else if(bits[0] && bits[1])
+                    } else if(bits[0] && bits[1])//00
                     { 
                         bits[0] = false;
                         bits[1] = false;
@@ -85,30 +98,19 @@ namespace notify
             }).Start();
             
             main();
-            Console.ReadKey();
+            Console.ReadKey();//After which the program exits
         }
         public static void main()
         {
 
-            WebClient webClient = new();
-            //webClient.DownloadFileCompleted += FileDONE;
-            //new Task(() =>
-            //{
-            webClient.DownloadFileCompleted += (e, eir) =>
-              {
-
-              };
-              webClient.DownloadFile(new Uri(URL), "kge.t");
-            runsThread = false;
-            Thread.Sleep(300);
+            WebClient webClient = new();//I know it's a outdate library but it still works awesome
+              webClient.DownloadFile(new Uri(URL), "kge.t"); // By the way, this works synchronously
+            runsThread = false;// So this will only happen once the file has downloaded
+            Thread.Sleep(300);// To give the thread some time to stop
 
             Console.Clear();
             Console.WriteLine("Done!!!");
             Console.CursorVisible = false;
-
-            //webClient.DownloadFileCompleted += FileDONE;
-            //}).Start();
-            //webClient.BaseAddress = "https://raw.githubusercontent.com/S-Dhir/blog/main/webmap.txt";
         }
     }
 }
